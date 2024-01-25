a10af877fe36d7a5c8cb307efd5a439eda716c7d
xelland042 <xelland042@gmail.com>
2024-01-25 14:49:32 +0500

    adding test for BookListCreate and actions for it

--- book/tests.py
+++ book/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.auth import get_user_model
-from book.views import BookListCreateApiView
+from book.models import Book
 
 User = get_user_model()
 
@@ -19,6 +19,13 @@ class TestBookListCreateApiView(TestCase):
             'user': self.user.id,
             'description': 'A test book description',
         }
+        self.book = Book.objects.create(
+            title='Existing Book',
+            price='19.99',
+            author='Existing Author',
+            user=self.user,
+            description='An existing book description',
+        )
 
     def test_create_book(self):
         response = self.client.post('', self.book_data, format='json')
@@ -28,4 +35,22 @@ class TestBookListCreateApiView(TestCase):
     def test_list_books(self):
         response = self.client.get('', format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Existing Book')
+
+    def test_retrieve_book(self):
+        response = self.client.get(f'/{self.book.slug}/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Existing Book')
+
+    def test_update_book(self):
+        updated_data = {'title': 'Updated Book', 'price': '25.99', 'author': 'Updated Author'}
+        response = self.client.patch(f'/{self.book.slug}/', updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.get(slug=self.book.slug).title, 'Updated Book')
+
+    def test_delete_book(self):
+        response = self.client.delete(f'/{self.book.slug}/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+        self.assertFalse(Book.objects.filter(slug=self.book.slug).exists())
